ad076af941c9065fd5c042c43858380def2c6dd6
Al4st0r12 <136027669+Al4st0r12@users.noreply.github.com>
2026-09-09 21:14:12 +0200

    Enhance backpack management with weight support
    Refactor backpack functionality to include weight calculation and update status handling.

--- client/client.lua
+++ client/client.lua
@@ -3,42 +3,52 @@ ESX = exports['es_extended']:getSharedObject()
 local hasBackpack = false
 local currentBackpack = nil
 
+local function GetWeightInKg(weightBonus)
+    local bonus = tonumber(weightBonus) or 0
+    if Config.WeightUnit == 'g' then
+        return bonus / 1000
+    end
+    return bonus
+end
 
 CreateThread(function()
     while not ESX.IsPlayerLoaded() do
         Wait(500)
     end
     
+    Wait(2000)
+    
     ESX.TriggerServerCallback('ncc_backpack:getBackpackStatus', function(backpack)
         if backpack then
             hasBackpack = true
             currentBackpack = backpack
             if Config.Debug then
-                print('[NCC-BACKPACK] Rucksack Status geladen:', backpack.label)
+                local weightInKg = GetWeightInKg(backpack.weight_bonus)
+                print(string.format('[NCC-BACKPACK] Rucksack Status geladen: %s (+%skg)', 
+                    backpack.label, weightInKg))
+            end
+        else
+            if Config.Debug then
+                print('[NCC-BACKPACK] Kein gespeicherter Rucksack gefunden')
             end
         end
     end)
 end)
 
-
-RegisterNetEvent('ncc_backpack:useBackpack', function(itemName, backpack)
-    if hasBackpack then
-        
-        TriggerServerEvent('ncc_backpack:removeBackpack')
-        hasBackpack = false
-        currentBackpack = nil
-    else
-        
-        TriggerServerEvent('ncc_backpack:equipBackpack', itemName, backpack)
-        hasBackpack = true
-        currentBackpack = {
-            item = itemName,
-            weight_bonus = backpack.weight_bonus,
-            label = backpack.label
-        }
+RegisterNetEvent('ncc_backpack:updateStatus', function(equipped, backpack)
+    hasBackpack = equipped
+    currentBackpack = backpack
+    
+    if Config.Debug then
+        if equipped then
+            local weightInKg = GetWeightInKg(backpack.weight_bonus)
+            print(string.format('[NCC-BACKPACK] Status aktualisiert: %s (+%skg)', 
+                backpack.label, weightInKg))
+        else
+            print('[NCC-BACKPACK] Rucksack entfernt')
+        end
     end
 end)
-
 
 RegisterCommand('removebackpack', function()
     if not hasBackpack then
@@ -47,20 +57,27 @@ RegisterCommand('removebackpack', function()
     end
     
     TriggerServerEvent('ncc_backpack:removeBackpack')
-    hasBackpack = false
-    currentBackpack = nil
 end, false)
-
 
 RegisterCommand('backpackstatus', function()
     if hasBackpack and currentBackpack then
-        local weightInKg = currentBackpack.weight_bonus / 1000
-        ESX.ShowNotification(string.format('Aktueller Rucksack: %s (+%skg)', currentBackpack.label, weightInKg))
+            local weightInKg = GetWeightInKg(currentBackpack.weight_bonus)
+        ESX.ShowNotification(string.format(Config.Messages['backpack_status'], 
+            currentBackpack.label, weightInKg))
     else
         ESX.ShowNotification(Config.Messages['no_backpack'])
     end
 end, false)
 
+RegisterCommand('rucksackstatus', function()
+    if hasBackpack and currentBackpack then
+            local weightInKg = GetWeightInKg(currentBackpack.weight_bonus)
+        ESX.ShowNotification(string.format(Config.Messages['backpack_status'], 
+            currentBackpack.label, weightInKg))
+    else
+        ESX.ShowNotification(Config.Messages['no_backpack'])
+    end
+end, false)
 
 function GetCurrentBackpack()
     return currentBackpack
@@ -70,5 +87,16 @@ function HasBackpack()
     return hasBackpack
 end
 
+function GetBackpackWeightBonus()
+    if hasBackpack and currentBackpack then
+        return currentBackpack.weight_bonus
+    end
+    return 0
+end
+
 exports('GetCurrentBackpack', GetCurrentBackpack)
 exports('HasBackpack', HasBackpack)
+exports('GetBackpackWeightBonus', GetBackpackWeightBonus)
+
+print("^0[^3NCC-Service^0] ^2Backpack Client successfully started!^0")
+print("^0[^3NCC-Service^0] ^3discord.gg/XCRAPKm9uP^0")
